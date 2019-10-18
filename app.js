bd2efb1431c1db2f874f0b1dd9f7c61b930c950f
Joshua Kimbareeba <jkimbareeba@yahoo.com>
2019-10-19 02:56:48 +0300

    first-commit

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+
+const app = express();
+
+const morgan = require('morgan');
+const bodyParser= require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.Promise = require('bluebird');
+
+
+mongoose.connect('mongodb+srv://jkimbareeba:' + process.env.MONGO_ATLAS_PW + '@akilidb-ovu0c.mongodb.net/akili?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true
+    }
+);
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//creating headers to allow access transmission of data from server to the client
+app.use((req, res, next) => {
+    //on the * , there can be a link to the actual website.
+    res.header('Access-Control-Allow-Origin', '*');
+    //this is for which kind of headers we want to accept
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    //checks the incoming method request (method giving us access to the http request)
+    //the browser will always send an Option request
+    //cannot be avoided
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT', 'POST', 'PATCH', 'DELETE', 'GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
+//error handling on the request thus a 404 error
+app.use((req, res, next) => {
+    const error = new Error('not found');
+    error.status = 404;
+    next(error);
+});
+
+//error handling especially on failure of operations
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const app = require('./app');
+
+
+const port = process.env.Port || 5000;
+
+const server = http.createServer(app);
+
+server.listen(port);
